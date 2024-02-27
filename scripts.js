137a3e72ff00d98922ff644e6150d97c90c84994
dm1tr <140421001+dm3tr-0@users.noreply.github.com>
2024-02-27 20:44:04 +0500

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,11 +1,1 @@
-document.getElementById('urfu-form').addEventListener('submit', function(event) {
-  event.preventDefault();
 
-  const nameInput = document.getElementById('name');
-
-  if (nameInput.value.trim() === '') {
-    nameInput.classList.add('error');
-  } else {
-    nameInput.classList.remove('error');
-  }
-});
